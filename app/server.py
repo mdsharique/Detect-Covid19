b42f76d2e58b0e96c367ac2c4b3bcfae1ef139d0
MD SHARIQUE <nukles1.07@gmail.com>
2020-04-17 18:05:25 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/7m886h0tgvumw44/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/7m886h0tgvumw44/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['COVID-19-Patient', 'Normal-Patient', 'Viral-Pneumonia-Patient']
